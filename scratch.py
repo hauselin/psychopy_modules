91aac0cb92f54424119cc39bf9de706b4d4a1979
Hause Lin <hauselin@gmail.com>
2019-10-24 14:41:08 -0400

    clean up

--- scratch.py
+++ scratch.py
@@ -7,7 +7,6 @@ x = 55
 
 
 y = 5
-<<<<<<< HEAD
 print("hello world")
 
 
@@ -16,25 +15,3 @@ print("hello world")
 
 z = 'hey include me'
 print(z)
-=======
-print('hello world')
-<<<<<<< HEAD
-
-
-
-
-
-qe = 1
-print(qe)
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> 6c45634d480b0bc06eff5a15e52d8575ee9df6e6
->>>>>>> 8a00d47f469bfa18595a5f38e8ba86da9616bc1e
